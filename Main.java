e34a1df2b32cdd24f1b0446506f7e5559a949cc3
Alex12345678902 <56139315+Phos202@users.noreply.github.com>
2021-09-17 12:52:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+
+ Scanner scan = new Scanner(System.in);
+    /* Write your code here */
+    
+    System.out.print("Please enter two integers:");
+    
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    
+    double x = (double)(a + b) / 2;
+System.out.println("The average is: " + x);
+
+
+
+
+
+  }
+}
